e7d79418e9f2e4550902961066359bca38e5834b
Tygo Egmond <35234103+tygoegmond@users.noreply.github.com>
2025-07-23 14:12:26 +0200

    wip

--- src/InertiaTablesServiceProvider.php
+++ src/InertiaTablesServiceProvider.php
@@ -10,11 +10,6 @@ class InertiaTablesServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('inertia-tables')
             ->hasRoutes('web')

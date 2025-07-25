22611fa2fbc026af73a944b722729da975b295b9
Tygo Egmond <35234103+tygoegmond@users.noreply.github.com>
2025-07-25 14:12:01 +0200

    wip

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -13,25 +13,25 @@ export default defineConfig({
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/tygoegmond/inertia-tables' }
 			],
 			sidebar: [
-				{ label: 'Installation', slug: '01-installation' },
-				{ label: 'Getting Started', slug: '02-getting-started' },
+				{ label: 'Installation', slug: 'installation' },
+				{ label: 'Getting Started', slug: 'getting-started' },
 				{
 					label: 'Columns',
 					items: [
-						{ label: 'Getting Started', slug: '03-columns/01-getting-started' },
-						{ label: 'Text Column', slug: '03-columns/02-text-column' },
+						{ label: 'Getting Started', slug: 'columns/getting-started' },
+						{ label: 'Text Column', slug: 'columns/text-column' },
 					],
 				},
-				{
-					label: 'Actions',
-					items: [
-						{ label: 'Getting Started', slug: '04-actions/01-getting-started' },
-						{ label: 'Row Actions', slug: '04-actions/02-row-actions' },
-						{ label: 'Bulk Actions', slug: '04-actions/03-bulk-actions' },
-						{ label: 'Header Actions', slug: '04-actions/04-header-actions' },
-						{ label: 'Custom Actions', slug: '04-actions/05-custom-actions' },
-					],
-				},
+                { label: 'Actions', slug: 'actions' },
+				// {
+				// 	label: 'Actions',
+				// 	items: [
+				// 		{ label: 'Getting Started', slug: 'actions/getting-started' },
+				// 		{ label: 'Row Actions', slug: 'actions/row-actions' },
+				// 		{ label: 'Bulk Actions', slug: 'actions/bulk-actions' },
+				// 		// { label: 'Header Actions', slug: 'actions/header-actions' },
+				// 	],
+				// },
 			],
 		}),
 	],

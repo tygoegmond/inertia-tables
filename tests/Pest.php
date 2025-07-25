5d8affb808720e1837f55dbdfa1f86d932c56464
tygoegmond <35234103+tygoegmond@users.noreply.github.com>
2025-07-25 08:10:33 +0000

    chore: fix styling issues

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,13 +1,13 @@
 <?php
 
-use Egmond\InertiaTables\Tests\TestCase;
+use Egmond\InertiaTables\Actions\Action;
+use Egmond\InertiaTables\Actions\BulkAction;
+use Egmond\InertiaTables\Contracts\HasTable;
 use Egmond\InertiaTables\Http\Requests\ActionRequest;
 use Egmond\InertiaTables\Table;
 use Egmond\InertiaTables\TableResult;
-use Egmond\InertiaTables\Contracts\HasTable;
 use Egmond\InertiaTables\Tests\Database\Models\User;
-use Egmond\InertiaTables\Actions\Action;
-use Egmond\InertiaTables\Actions\BulkAction;
+use Egmond\InertiaTables\Tests\TestCase;
 use Illuminate\Http\Request;
 
 uses(TestCase::class)->in(__DIR__);
@@ -20,7 +20,7 @@ function createActionRequest(array $data = []): ActionRequest
     // Separate query parameters (like 'record') from POST data (like 'records')
     $queryParams = [];
     $postData = [];
-    
+
     foreach ($data as $key => $value) {
         if ($key === 'record') {
             $queryParams[$key] = $value;
@@ -28,9 +28,9 @@ function createActionRequest(array $data = []): ActionRequest
             $postData[$key] = $value;
         }
     }
-    
-    $request = Request::create('/test?' . http_build_query($queryParams), 'POST', $postData);
-    
+
+    $request = Request::create('/test?'.http_build_query($queryParams), 'POST', $postData);
+
     return ActionRequest::createFrom($request);
 }
 

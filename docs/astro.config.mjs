f8cc94e351078776297a8fc413b43f3421cc6936
Tygo Egmond <35234103+tygoegmond@users.noreply.github.com>
2025-07-25 12:19:27 +0200

    wip

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import starlightThemeRapide from 'starlight-theme-rapide'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-            plugins: [starlightThemeFlexoki()],
+            plugins: [starlightThemeRapide()],
 			title: 'Inertia Tables',
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/tygoegmond/inertia-tables' }

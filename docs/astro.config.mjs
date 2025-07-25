d5d52380dafc23abe5e7c4331479c342c0412271
Tygo Egmond <35234103+tygoegmond@users.noreply.github.com>
2025-07-25 11:41:25 +0200

    wip

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -9,17 +9,8 @@ export default defineConfig({
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+                { label: 'Installation', slug: 'installation' },
+                // { label: 'Getting Started', slug: 'getting-started' },
 			],
 		}),
 	],
